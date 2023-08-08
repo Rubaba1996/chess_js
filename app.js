f6ffc22860505e530311b5122c6b7b1e06db8fe1
rubaba.nagiyeva@mail.ru <rubaba.nagiyeva@mail.ru>
2023-08-08 12:58:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let table = document.querySelector('table');
+
+let black = document.querySelectorAll('.black');
+black.forEach(e=>{
+    e.style.backgroundColor="#555";
+});
+
+let white = document.querySelectorAll('.white');
+white.forEach(a=>{
+    a.style.backgroundColor="antiquewhite";
+});
